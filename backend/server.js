8192b73f795fc0de50d75b7d39bd44b5e1af82a4
Abhi Ram <abhiramallada9999@gmail.com>
2026-04-14 21:21:09 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -22,5 +22,5 @@ connectDB();
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on https://Task-Manager-backend.onrender.com`);
 });
