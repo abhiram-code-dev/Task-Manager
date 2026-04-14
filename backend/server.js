251ab2e2714ede1ab47592f5280fa8969402224f
Abhi Ram <abhiramallada9999@gmail.com>
2026-04-14 21:57:39 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+app.use(cors());
 const connectDB = require("./config/db");
 const path = require("path");
+
 
 const authRoutes = require("./routes/auth");
 const taskRoutes = require("./routes/tasks");
@@ -19,9 +21,15 @@ app.use("/tasks", taskRoutes); // Tasks base path
 
 // Connect Database
 connectDB();
+// Serve frontend files
+app.use(express.static(path.join(__dirname, "../frontend")));
 
+// Load index.html by default
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/index.html"));
+});
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server is running on https://Task-Manager-backend.onrender.com`);
+  console.log(`Server is running on port ${PORT}`);
 });
